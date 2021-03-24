3b50f46e675b3fdfaf98bfa082f20187f904e6d9
JaehyunPark-S <show6756@gmail.com>
2021-03-25 00:19:47 +0900

    User modeling

---
+++ users/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
+
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+
+    """ Custom User Admin """
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Cunstom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                )
+            }
+        ),
+    )
