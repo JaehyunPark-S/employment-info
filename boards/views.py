6a43541c17b65ef1cfdd3b15de70480bb7df4c0c
JaehyunPark-S <show6756@gmail.com>
2021-03-28 00:51:42 +0900

    Pagination to Infinite Scroll

--- boards/views.py
+++ boards/views.py
@@ -7,6 +7,6 @@ class HomeView(ListView):
     """ HomeView Definition """
 
     model = models.Board
+    template_name = "board_list.html"
     paginate_by = 10
-    paginate_orphans = 5
     ordering = "created"
